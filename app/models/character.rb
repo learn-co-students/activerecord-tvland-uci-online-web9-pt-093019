eaec4cd00905fa5b3cca8d25793367800feeef79
Justin Phillips <justinphil02@yahoo.com>
2019-12-22 03:32:59 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
+  
+  def say_that_thing_you_say
+    phrase = self.catchphrase 
+    char = self.name 
+    "#{char} always says: #{phrase}"
+  end 
+  
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  has_many :characters 
+  has_many :actors, through: :characters
+  belongs_to :network 
   
+  def actors_list 
+    list = []
+    Actor.all.each do |actor|
+      list << actor.full_name if actor.shows.include?(self) 
+    end 
+    list 
+  end 
 end
