bc903586715b9d3dadf77837050378c9578251d6
Andrew Capp <acapp909@gmail.com>
2019-12-28 01:04:45 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return_object = Actor.find(self.id)
+    return_object.first_name + " " + return_object.last_name
+  end
+
+  def list_roles
+    self.characters.collect do |role|
+      list_return = "#{role.name} - #{role.show.name}"
+    end
+  end
 end
